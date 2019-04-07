7490123e34221dc4666ee25e19ed7d745b7fb7cc
Woz4tetra <woz4tetra@gmail.com>
2019-04-06 23:11:14 -0400

    Laser is detected laptop lid and adding it to the costmap as obstacles. Need to create a laser range filter. Adjusted some move_base parameters. Fixed up joystick controls for controlling move_base and lights.

--- earth_rover_teleop/src/earth_rover_teleop_node.py
+++ earth_rover_teleop/src/earth_rover_teleop_node.py
@@ -7,8 +7,10 @@ import rospy
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Float64, Bool
+from actionlib_msgs.msg import GoalID
 
 from earth_rover_microcontroller_bridge.srv import *
+
 
 class EarthRoverTeleop:
     def __init__(self):
@@ -23,6 +25,28 @@ class EarthRoverTeleop:
         self.e_stop_msg = Twist()
         self.is_e_stopped = False
         self.prev_joy_msg = None
+        self.cancel_goal_msg = GoalID()
+        self.current_pattern_index = 0
+        self.light_patterns = ["w", "rs", "rf"]
+        self.lights_are_on = True
+
+        # button mapping:
+        # 0: A,    1: B,     2: X,      3: Y
+        # 4: L1,   5: R1,    6: Select, 7: Start
+        # 8: Home, 9: L joy, 10: R joy
+        self.buttons = {
+            "A": 0,
+            "B": 1,
+            "X": 2,
+            "Y": 3,
+            "L1": 4,
+            "R1": 5,
+            "Select": 6,
+            "Start": 7,
+            "Home": 8,
+            "L joy": 9,
+            "R joy": 10
+        }
 
         # parameters from launch file
         self.linear_scale = rospy.get_param("~linear_scale", 1.0)
@@ -33,6 +57,8 @@ class EarthRoverTeleop:
         self.twist_publish_topic_name = rospy.get_param("~twist_publish_topic_name", "cmd_vel")
         self.left_led_control_service_name = rospy.get_param("~left_led_control_service_name", "left/left_encoder/led_control_left")
         self.right_led_control_service_name = rospy.get_param("~right_led_control_service_name", "right/right_encoder/led_control_right")
+        self.enable_move_base_hookups = rospy.get_param("~enable_move_base_hookups", False)
+        self.cancel_move_base_goal_topic_name = "/move_base/cancel"
 
         # publishing topics
         self.cmd_vel_pub = None
@@ -48,6 +74,11 @@ class EarthRoverTeleop:
         self.e_stop_lock_pub = rospy.Publisher("e_stop_lock", Bool, queue_size=5)
         self.e_stop_pub = rospy.Publisher("e_stop", Twist, queue_size=5)
 
+        if self.enable_move_base_hookups:
+            self.cancel_goal_pub = rospy.Publisher(self.cancel_move_base_goal_topic_name, GoalID, queue_size=1)
+        else:
+            self.cancel_goal_pub = None
+
         # subscription topics
         self.joy_sub = rospy.Subscriber("/joy", Joy, self.joystick_msg_callback, queue_size=5)
 
@@ -62,7 +93,6 @@ class EarthRoverTeleop:
 
         self.left_led_control_service = rospy.ServiceProxy(self.left_led_control_service_name, LedControl)
         self.right_led_control_service = rospy.ServiceProxy(self.right_led_control_service_name, LedControl)
-
         rospy.loginfo("Joystick E stop is mapped to the Y button")
 
     def did_button_change(self, msg, index):
@@ -77,22 +107,30 @@ class EarthRoverTeleop:
         # 0: A,    1: B,     2: X,      3: Y
         # 4: L1,   5: R1,    6: Select, 7: Start
         # 8: Home, 9: L joy, 10: R joy
-        if self.did_button_change(msg, 0):
-            self.control_leds("o")
+        if self.did_button_change(msg, self.buttons["A"]):  # toggle lights
+            self.lights_are_on = not self.lights_are_on
+            if self.lights_are_on:
+                self.set_led_pattern()
+            else:
+                self.control_leds("o")
 
-        elif self.did_button_change(msg, 1):
-            self.control_leds("w")
+        elif self.did_button_change(msg, self.buttons["B"]):  # switch light pattern
+            if self.lights_are_on:
+                self.set_next_pattern()
+                self.set_led_pattern()
 
-        elif self.did_button_change(msg, 2):
-            self.control_leds("rs")
-
-        elif self.did_button_change(msg, 3):
+        elif self.did_button_change(msg, self.buttons["R1"]):
             self.is_e_stopped = not self.is_e_stopped
             self.e_stop_lock_msg.data = self.is_e_stopped
             if self.is_e_stopped:
                 rospy.logwarn("Joystick E stop was thrown!")
             else:
                 rospy.loginfo("Joystick E stop was released")
+
+        elif self.did_button_change(msg, self.buttons["L1"]):
+            if self.enable_move_base_hookups:
+                self.cancel_goal_pub.publish(self.cancel_goal_msg)
+                print "Canceling goal from joystick button press"
 
         linear_val = self.linear_scale * msg.axes[int(self.linear_axis)]
         angular_val = self.angular_scale * msg.axes[int(self.angular_axis)]
@@ -108,6 +146,14 @@ class EarthRoverTeleop:
 
         self.prev_joy_msg = msg
 
+    def set_led_pattern(self):
+        self.control_leds(self.light_patterns[self.current_pattern_index])
+
+    def set_next_pattern(self):
+        self.current_pattern_index += 1
+        if self.current_pattern_index >= len(self.light_patterns):
+            self.current_pattern_index = 0
+
     def control_leds(self, command):
         try:
             self.left_led_control_service(command)
@@ -118,15 +164,16 @@ class EarthRoverTeleop:
 
     def run(self):
         clock_rate = rospy.Rate(30)
-
         prev_time = rospy.get_rostime()
+        self.set_led_pattern()
+
         while not rospy.is_shutdown():
             if self.is_e_stopped:
                 self.e_stop_pub.publish(self.e_stop_msg)
 
                 now = rospy.get_rostime()
                 if now - prev_time > rospy.Duration(1):
-                    rospy.logwarn("Joystick E stop is thrown. Press Y to release")
+                    rospy.logwarn("Joystick E stop is thrown. Press R1 to release")
                     prev_time = now
 
             self.e_stop_lock_pub.publish(self.e_stop_lock_msg)
